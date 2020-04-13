cb4cad65261fbc16dabe3914e479dc51862b5c52
Jordan Handy <handy.jordan@gmail.com>
2020-04-13 07:07:46 -0400

    code refactored

---
+++ runner.py
@@ -0,0 +1,6 @@
+import subprocess
+import citer
+
+program ="Z:\Google Drive Sync - Trent\Programming\Systems Development\Python\citer\citer.py"
+arguments = "&> log.txt"
+subprocess.call([program,arguments])
